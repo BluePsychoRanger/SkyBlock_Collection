89f345418d8a0b2aed6d93c49f78fb68c0eb70e3
BluePsychoRanger <40612230+BluePsychoRanger@users.noreply.github.com>
2023-08-23 03:03:13 -0700

    Simple Beet Pipeline
    - `beet` will build the dev version into the `out` folder
    - `beet -p ./beet-release.yaml` will build the packs into the `build` folder
    - `pack.mcmeta` is auto generated
    - README and LICENSE are included in download

---
+++ beet_plugins/output.py
@@ -0,0 +1,69 @@
+from beet import Context
+from bolt import Module
+import os
+
+def beet_default(ctx: Context):
+	"""Saves the datapack to the ./out folder."""
+	version = os.getenv("VERSION", "1.20")
+	out_dir = str(ctx.directory.parent).replace('src','out')
+
+	ctx.data.pack_format = 15 # manually set as beet's `latest` is not available for 1.20 at this time.
+	ctx.data.description = [
+		"",
+		{
+			"text": f"{ctx.project_name}",
+			"color": "light_purple"
+		},
+		f"\nby ",
+		{
+			"text": "BluePsychoRanger",
+			"color": "blue"
+		}
+	]
+
+	ctx.data.mcmeta.data.update({
+		"version": ctx.project_version
+	})
+
+	ctx.data[Module].clear() # manually cleanup bolt modules
+
+	ctx.data.save(
+		path=out_dir + f"/{ctx.project_id}_{version.replace('.', '_')}",
+		overwrite=True,
+	)
+
+from beet import Context
+from pathlib import Path
+from bolt import Module
+import os
+
+def release(ctx: Context):
+	"""Saves the datapack to the ./build folder."""
+	version = os.getenv("VERSION", "1.20")
+	out_dir = str(ctx.directory.parent).replace('src','build')
+
+	ctx.data.pack_format = 15 # manually set as beet's `latest` is not available for 1.20 at this time.
+	ctx.data.description = [
+		"",
+		{
+			"text": f"{ctx.project_name}",
+			"color": "light_purple"
+		},
+		f"\nby ",
+		{
+			"text": "BluePsychoRanger",
+			"color": "blue"
+		}
+	]
+
+	ctx.data.mcmeta.data.update({
+		"version": ctx.project_version
+	})
+
+	ctx.data[Module].clear() # manually cleanup bolt modules
+
+	ctx.data.save(
+		path=out_dir + f"/{ctx.project_id}_{version.replace('.', '_')}",
+		overwrite=True,
+		zipped=True,
+	)
