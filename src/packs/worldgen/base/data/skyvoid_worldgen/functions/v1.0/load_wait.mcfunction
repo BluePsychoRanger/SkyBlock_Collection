bee35a067d7aeb4e0280d5fb75f8f3acbf2785b3
BluePsychoRanger <mcbluepsychoranger@gmail.com>
2023-11-08 01:08:00 -0800

    Make base build off of base_with_end

--- src/packs/worldgen/base/data/skyvoid_worldgen/functions/v1.0/load_wait.mcfunction
+++
@@ -1,2 +0,0 @@
-execute unless entity @a[limit=1] run schedule function skyvoid_worldgen:v1.0/load_wait 2t
-execute if entity @a[limit=1] run function skyvoid_worldgen:v1.0/load
--- src/plugins/worldgen/generate.py
+++ src/plugins/worldgen/generate.py
@@ -5,3 +5,6 @@ def beet_default(ctx: Context):
 
 def base(ctx: Context):
   ctx.require(subproject({'directory': f'../base', 'extend': 'beet.yaml'}))
+
+def base_with_end(ctx: Context):
+  ctx.require(subproject({'directory': f'../base_with_end', 'extend': 'beet.yaml'}))
