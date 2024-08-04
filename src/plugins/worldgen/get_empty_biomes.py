41ba705ea1a889b13d546b6517a74b754b76bdba
BluePsychoRanger <mcbluepsychoranger@gmail.com>
2024-08-04 12:50:59 -0700

    Optimize Scripts
    - use caching for empty generated biome and structure files
    - flatten empty biomes script to call a common function
    - fix some and ignore other typing errors (using strict typing)

--- src/plugins/worldgen/get_empty_biomes.py
+++ src/plugins/worldgen/get_empty_biomes.py
@@ -25,91 +25,60 @@ END_BIOMES = [
 ]
 
 def beet_default(ctx: Context):
-  vanilla = ctx.inject(Vanilla)
-  biomes = vanilla.mount("data/minecraft/worldgen").data[WorldgenBiome]
-
-  for biome in biomes.keys():
-    data = biomes[biome].data
-    name = biome.removeprefix("minecraft:")
-
-    # clear carvers
-    data["carvers"] = {}
-
-    # clear features
-    if (name in NETHER_BIOMES):
-      data["features"] = [[], [], [], [], [], [], [],
-                [], [], [f"{NAME}:purge/nether_fortress"]]
-    elif (name in END_BIOMES):
-      data["features"] = [[], [], [], [], [], [], [],
-                [], [], [], [f"{NAME}:purge/end_second_layer"]]
-      if (name == "end_highlands"):
-        data["features"][4] = ["minecraft:end_gateway_return"]
-      elif (name == "the_end"):
-        data["features"][4] = ["minecraft:end_spike"]
-    else:
-      flowers = []
-      step_9 = data["features"][9]
-      for feature in step_9:
-        if "flowers" in feature or "minecraft:flower" in feature:
-          flowers.append(feature)
-      data["features"] = [[], [], [], [], [], [], [], [], [],
-                flowers, [f"{NAME}:purge/overworld_second_layer"]]
-
-    ctx.data[f"minecraft:{name}"] = WorldgenBiome(data)
-
+  gen(ctx, "default")
 
 def empty(ctx: Context):
-  vanilla = ctx.inject(Vanilla)
-  biomes = vanilla.mount("data/minecraft/worldgen").data[WorldgenBiome]
-  for biome in biomes.keys():
-    data = biomes[biome].data
-    name = biome.removeprefix("minecraft:")
-
-    # clear carvers
-    data["carvers"] = {}
-
-    # clear features
-    if (name in NETHER_BIOMES):
-      data["features"] = [[], [], [], [], [], [], [],
-                [], [], [f"{NAME}:purge/nether_fortress"]]
-    elif (name in END_BIOMES):
-      data["features"] = [[], [], [], [], [], [], [],
-                [], [], [], [f"{NAME}:purge/end_second_layer"]]
-    else:
-      flowers = []
-      step_9 = data["features"][9]
-      for feature in step_9:
-        if "flowers" in feature or "minecraft:flower" in feature:
-          flowers.append(feature)
-      data["features"] = [[], [], [], [], [], [], [], [], [], flowers, [f"{NAME}:purge/overworld_second_layer"]]
-
-    ctx.data[f"minecraft:{name}"] = WorldgenBiome(data)
+  gen(ctx, "empty", full_removal=True)
 
 def normal_end(ctx: Context):
-  vanilla = ctx.inject(Vanilla)
-  biomes = vanilla.mount("data/minecraft/worldgen").data[WorldgenBiome]
+  gen(ctx, "normal_end", ["end"])
 
-  for biome in biomes.keys():
-    data = biomes[biome].data
-    name = biome.removeprefix("minecraft:")
 
-    if (name in END_BIOMES):
-      continue
 
-    # clear carvers
-    data["carvers"] = {}
 
-    # clear features
-    if (name in NETHER_BIOMES):
-      data["features"] = [[], [], [], [], [], [], [],
-                [], [], [f"{NAME}:purge/nether_fortress"]]
-    else:
-      flowers = []
-      step_9 = data["features"][9]
-      for feature in step_9:
-        if "flowers" in feature or "minecraft:flower" in feature:
-          flowers.append(feature)
-      data["features"] = [[], [], [], [], [], [], [], [], [],
-                flowers, [f"{NAME}:purge/overworld_second_layer"]]
+def gen(ctx: Context, cache_loc:str, ignored_dimensions: list[str] = [], full_removal: bool = False):
+  clear_overworld = "overworld" not in ignored_dimensions
+  clear_nether = "nether" not in ignored_dimensions
+  clear_end = "end" not in ignored_dimensions
 
-    ctx.data[f"minecraft:{name}"] = WorldgenBiome(data)
+  # if no dimensions are affected, don't to anything
+  if (not clear_overworld and not clear_nether and not clear_end):
+    return
+
+  with ctx.generate.draft() as draft:
+    # generate only upon cache miss
+    draft.cache(f"skyvoid/empty_biomes/{cache_loc}", "")
+
+    vanilla = ctx.inject(Vanilla)
+    biomes = vanilla.mount("data/minecraft/worldgen").data[WorldgenBiome]
+
+    for biome in biomes.keys():
+      data = biomes[biome].data
+      name = biome.removeprefix("minecraft:")
+
+      # clear carvers
+      data["carvers"] = {}
+
+      # clear features
+      if name in NETHER_BIOMES:
+        if clear_nether:
+          data["features"] = [[], [], [], [], [], [], [],
+                  [], [], [f"{NAME}:purge/nether_fortress"]]
+      elif name in END_BIOMES:
+        if clear_end:
+          data["features"] = [[], [], [], [], [], [], [],
+                  [], [], [], [f"{NAME}:purge/end_second_layer"]]
+          if not full_removal and (name == "end_highlands"):
+            data["features"][4] = ["minecraft:end_gateway_return"]
+          elif not full_removal and (name == "the_end"):
+            data["features"][4] = ["minecraft:end_spike"]
+      elif clear_overworld:
+        flowers:list[str] = []
+        step_9 = data["features"][9]
+        for feature in step_9:
+          if "flowers" in feature or "minecraft:flower" in feature:
+            flowers.append(feature)
+        data["features"] = [[], [], [], [], [], [], [], [], [],
+                  flowers, [f"{NAME}:purge/overworld_second_layer"]]
+
+      draft.data[f"minecraft:{name}"] = WorldgenBiome(data)
--- src/plugins/worldgen/get_empty_structures.py
+++ src/plugins/worldgen/get_empty_structures.py
@@ -1,7 +1,7 @@
 from beet import Context, Structure
 from beet.contrib.vanilla import Vanilla
 import re
-from nbtlib import *
+from nbtlib import * # type: ignore
 
 NAME = "skyvoid_worldgen"      # name of the module
 DIR = f"worldgen/{NAME}/data"
@@ -9,7 +9,7 @@ TEMP_PATH = f"worldgen/{NAME}/temp_files"
 BIOME_FOLDER = f"minecraft/worldgen/biome"
 
 def beet_default(ctx: Context):
-  target_structures = [
+  target_structures:list[str] = [
     # "ancient_city",
     # "bastion",
     # "end_city",
@@ -17,63 +17,63 @@ def beet_default(ctx: Context):
     "trial_chambers",
     # "woodland_mansion"
   ]
-  kept_pieces = [
+  kept_pieces:list[str] = [
     "end_city/ship"
   ]
-  kept_blocks = [
+  kept_blocks:list[str] = [
     "air",
     "cave_air",
     "void_air",
     "jigsaw",
     "structure_block"
   ]
-  kept_entities = [
+  kept_entities:list[str] = [
     "shulker",
     "allay"
   ]
-  gen(ctx, target_structures, kept_pieces, kept_blocks, kept_entities)
+  gen(ctx, target_structures, kept_pieces, kept_blocks, kept_entities, "default")
 
 
 
 def empty(ctx: Context):
-  target_structures = [
+  target_structures:list[str] = [
     # "ancient_city",
     # "bastion",
     # "pillager_outpost",
     "trial_chambers",
     # "woodland_mansion"
   ]
-  kept_pieces = []
-  kept_blocks = [
+  kept_pieces:list[str] = []
+  kept_blocks:list[str] = [
     "air",
     "cave_air",
     "void_air",
     "jigsaw"
   ]
-  kept_entities = []
-  gen(ctx, target_structures, kept_pieces, kept_blocks, kept_entities)
+  kept_entities:list[str] = []
+  gen(ctx, target_structures, kept_pieces, kept_blocks, kept_entities, "empty")
 
 
 
 def normal_end(ctx: Context):
-  target_structures = [
+  target_structures:list[str] = [
     # "ancient_city",
     # "bastion",
     # "pillager_outpost",
     "trial_chambers",
     # "woodland_mansion"
   ]
-  kept_pieces = []
-  kept_blocks = [
+  kept_pieces:list[str] = []
+  kept_blocks:list[str] = [
     "air",
     "cave_air",
     "void_air",
     "jigsaw"
   ]
-  kept_entities = [
+  kept_entities:list[str] = [
     "allay"
   ]
-  gen(ctx, target_structures, kept_pieces, kept_blocks, kept_entities)
+  gen(ctx, target_structures, kept_pieces, kept_blocks, kept_entities, "normal_end")
 
 
 
@@ -85,44 +85,48 @@ def normal_end(ctx: Context):
 
 
 
-def gen(ctx:Context, target_structures:list[str], kept_pieces:list[str], kept_blocks:list[str], kept_entities:list[str]):
-  vanilla = ctx.inject(Vanilla)
-  structures = vanilla.mount("data/minecraft").data[Structure]
+def gen(ctx:Context, target_structures:list[str], kept_pieces:list[str], kept_blocks:list[str], kept_entities:list[str], cache_loc:str):
+  with ctx.generate.draft() as draft:
+    # generate only upon cache miss
+    draft.cache(f"skyvoid/empty_structures/{cache_loc}", "")
 
-  for structure in structures.keys():
-    data = structures[structure].data
-    name = structure.removeprefix("minecraft:")
-    folder = re.split("/", name)[0]
+    vanilla = ctx.inject(Vanilla)
+    structures = vanilla.mount("data/minecraft").data[Structure]
 
-    # find structures files to modify
-    if folder in target_structures and name not in kept_pieces:
-      # update block palette to keep only certain blocks
-      palette_swap:list[int] = []
-      new_palette:List = []
-      palette = data['palette']
-      for i, b in enumerate(palette):
-        if b['Name'].removeprefix("minecraft:") in kept_blocks:
-          palette_swap.append(i)
-          new_palette.append(b)
-      data['palette'] = new_palette
+    for structure in structures.keys():
+      data = structures[structure].data
+      name = structure.removeprefix("minecraft:")
+      folder = re.split("/", name)[0]
 
-      # update block definitions to new palette
-      new_blocks = []
-      for block in data['blocks']:
-        if block['state'] in palette_swap:
-          block['state'] = Int(palette_swap.index(block['state']))
-          # change final state of all jigsaws 
-          if 'nbt' in block.keys() and 'final_state' in block['nbt'].keys():
-            block['nbt']['final_state'] = String("minecraft:air")
-          new_blocks.append(block)
-      data['blocks'] = new_blocks
+      # find structures files to modify
+      if folder in target_structures and name not in kept_pieces:
+        # update block palette to keep only certain blocks
+        palette_swap:list[int] = []
+        new_palette:List = [] # type: ignore
+        palette = data['palette'] # type: ignore
+        for i, b in enumerate(palette): # type: ignore
+          if b['Name'].removeprefix("minecraft:") in kept_blocks: # type: ignore
+            palette_swap.append(i)
+            new_palette.append(b) # type: ignore
+        data['palette'] = new_palette
 
-      # only keep certain entities
-      new_entities = []
-      for entity in data['entities']:
-        if entity['nbt']['id'].removeprefix("minecraft:") in kept_entities:
-          new_entities.append(entity)
-      data['entities'] = new_entities
+        # update block definitions to new palette
+        new_blocks = []
+        for block in data['blocks']: # type: ignore
+          if block['state'] in palette_swap:
+            block['state'] = Int(palette_swap.index(block['state'])) # type: ignore
+            # change final state of all jigsaws 
+            if 'nbt' in block.keys() and 'final_state' in block['nbt'].keys(): # type: ignore
+              block['nbt']['final_state'] = String("minecraft:air")
+            new_blocks.append(block) # type: ignore
+        data['blocks'] = new_blocks
 
-      # add structure to datapack
-      ctx.data[f"minecraft:{name}"] = Structure(data)
+        # only keep certain entities
+        new_entities = []
+        for entity in data['entities']: # type: ignore
+          if entity['nbt']['id'].removeprefix("minecraft:") in kept_entities: # type: ignore
+            new_entities.append(entity) # type: ignore
+        data['entities'] = new_entities
+
+        # add structure to datapack
+        draft.data[f"minecraft:{name}"] = Structure(data)
