58497cfff71df615aec95843c1f899cde8d56bbe
BluePsychoRanger <mcbluepsychoranger@gmail.com>
2024-08-04 11:21:53 -0700

    Fix One Block Small Island Detection
    - fix predicate that checks for a small snowy taiga

--- src/packs/starter_islands/skyvoid_vanilla_oneblock/generate_predicate.py
+++ src/packs/starter_islands/skyvoid_vanilla_oneblock/generate_predicate.py
@@ -15,25 +15,6 @@ def generate_predicate():
           "offsetY": y,
           "offsetZ": z,
           "predicate": {
-            "biome": "minecraft:snowy_taiga"
-          }
-        }
-        predicate.append(new)
-
-  return predicate
-
-def generate_overlay_predicate():
-  predicate = []
-
-  for x in range(-radius,radius+1):
-    for z in range(-radius,radius+1):
-      for y in range(height):
-        new = {
-          "condition": "minecraft:location_check",
-          "offsetX": x,
-          "offsetY": y,
-          "offsetZ": z,
-          "predicate": {
             "biomes": "minecraft:snowy_taiga"
           }
         }
@@ -43,4 +24,3 @@ def generate_overlay_predicate():
 
 def beet_default(ctx: Context):
   ctx.data["skyvoid_vanilla_oneblock:in_snowy_taiga"] = Predicate(generate_predicate())
-  ctx.data.overlays["skyvoid_1_20_5"]["skyvoid_vanilla_oneblock:in_snowy_taiga"] = Predicate(generate_overlay_predicate())
